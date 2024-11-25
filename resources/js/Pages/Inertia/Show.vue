1c639560b9809c2443ad4677ce463afdaf44e66a
kawamoto <krt34kk0@gmail.com>
2024-11-25 23:01:52 +0900

    inertia-vue.js

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import {Link} from '@inertiajs/vue3';
+    import { Inertia } from '@inertiajs/inertia'
+
+    defineProps({
+        id : String,
+        blog: Object
+    })
+
+     const deleteConfirm = (id) =>{
+        Inertia.delete(`/inertia/${id}`,{
+            onBefore:()=> confirm('本当に削除しますか?')
+        })
+     }
+
+</script>
+
+<template>
+    showテスト<br>
+    <Link href='/'>{{ id  }}</Link>
+    {{ id }}<br>
+    {{ blog.title }}<br>
+    <button @click="deleteConfirm(blog.id)">削除</button>
+</template>
